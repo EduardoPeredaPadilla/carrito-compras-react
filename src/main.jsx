f29f06cdf0be114fcde15e9f7b497227ddec1229
EduardoPeredaPadilla <eduardoperedapadilla@gmail.com>
2023-10-02 17:55:18 -0600

    Carrito de compras

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CarritoApp } from './CarritoApp'
+// import './styles/card.css'
+// import './styles/lista.css'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <CarritoApp />
+    </React.StrictMode>
+  </BrowserRouter>
+)
